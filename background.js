755f5461fa519eb3196ecec69a33f3820dadcc0f
superkevin@gmail.com <superkevin@gmail.com>
2014-06-15 14:16:24 -0400

    play/pause works via page action or via cmd+shift+8

--- background.js
+++ background.js
@@ -17,15 +17,32 @@ chrome.runtime.onInstalled.addListener(function() {
   });
 });
 
+var playIcon = {
+  '19': 'play.png',
+  '38': 'play@2x.png'
+};
+
+var pauseIcon = {
+  '19': 'pause.png',
+  '38': 'pause@2x.png'
+};
+
 function handlePlayPause(tabId) {
   chrome.tabs.executeScript(tabId, {
-    code: "document.getElementsByClassName('miniplayer-control-play-pause')[0].click();"
+    code: "document.getElementsByClassName('miniplayer-control-play-pause')[0].click(); document.getElementsByClassName('player-state-pause').length > 0;"
+  }, function(results) {
+    console.log(results[0]);
+    if (results[0]) {
+      chrome.pageAction.setIcon({tabId: tabId, path: playIcon});
+    } else {
+      chrome.pageAction.setIcon({tabId: tabId, path: pauseIcon});
+    }
   });
 };
 
 chrome.commands.onCommand.addListener(function(command) {
   chrome.tabs.query({
-    'url': '*://songza.com/'
+    'url': '*://songza.com/*'
   }, function(tabs) {
     console.log(tabs);
     if (tabs) {
